1b0d596416c38cb70aa84b6b2581a16428c2ac97
JMMyles <JosephMMyles@gmail.com>
2014-11-01 16:12:49 -0700

    I finished the 'Define the Login RPC service' tutorial stage.  The next step is updating the UI, so I'm committing now incase something goes wrong in the VandelayIndustries.java class.  Since we're all messing with this class I might need to come back to it.  Try to put code in the loadVandelayIndustries method instead of the onModuleLoad method from now on.

---
+++ VandelayIndustries/src/com/ubc/cs310/client/LoginServiceAsync.java
@@ -0,0 +1,7 @@
+package com.ubc.cs310.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface LoginServiceAsync {
+  public void login(String requestUri, AsyncCallback<LoginInfo> async);
+}
