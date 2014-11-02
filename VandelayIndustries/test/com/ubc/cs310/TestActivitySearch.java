a6c32e97479990f89ea924a05cb22fe368c8a945
JMMyles <JosephMMyles@gmail.com>
2014-11-01 21:38:40 -0700

    I added a pair of test classes.  They aren't compiling yet because of a lack of Space object.  Create a Space object then run the tests.

---
+++ VandelayIndustries/test/com/ubc/cs310/TestActivitySearch.java
@@ -0,0 +1,65 @@
+package com.ubc.cs310;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.ubc.cs310.server.SpaceServiceImpl;
+
+public class TestActivitySearch {
+
+	// Create Spaces here
+	space1 = new Space();
+	space2 = new Space();
+	space3 = new Space();
+	
+	// Create parameters here
+	String noMatches = "Card";
+	String param1 = "";
+	String param2 = "";
+	
+	// Test empty list of Spaces (this shouldn't happen in reality)
+	@Test
+	private void testEmptyActivitySearch() {
+		List<Space> emptyActivityList = new ArrayList<Space>();
+		assertEquals(emptyActivityList, SpaceServiceImpl.activitySearch(noMatches, emptyActivityList));
+	}
+	
+	// Test empty string as parameter expect full list returned
+	@Test
+	private void testEmptyParamActivitySearch() {
+		List<Space> activityList = new ArrayList<Space>();
+		activityList.add(space1);
+		assertEquals(activityList, SpaceServiceImpl.activitySearch("", activityList));
+	}
+	
+	@Test
+	private void testOneActivitySearch() {
+		List<Space> activityList = new ArrayList<Space>();
+		activityList.add(space1);
+		activityList.add(space2);
+		activityList.add(space3);
+		List<Space> resultList = new ArrayList<Space>();
+		resultList.add(space1);
+		assertEquals(resultList, SpaceServiceImpl.activitySearch(param1, activityList));
+	}
+	
+	@Test
+	private void testTwoActivitySearch() {
+		List<Space> activityList = new ArrayList<Space>();
+		activityList.add(space1);
+		activityList.add(space2);
+		activityList.add(space3);
+		List<Space> resultList = new ArrayList<Space>();
+		resultList.add(space1);
+		resultList.add(space2);
+		assertEquals(resultList, SpaceServiceImpl.activitySearch(param2, activityList));
+	}
+	
+	@Test
+	private void testInvalidActivitySearch() {
+		List<Space> activityList = new ArrayList<Space>();
+		activityList.add(space1);
+		List<Space> resultList = new ArrayList<Space>();
+		assertEquals(resultList, SpaceServiceImpl.activitySearch(noMatches, activityList));
+	}
+}
+
---
+++ VandelayIndustries/test/com/ubc/cs310/TestTitleSearch.java
@@ -0,0 +1,64 @@
+package com.ubc.cs310;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.ubc.cs310.server.SpaceServiceImpl;
+
+public class TestTitleSearch {
+
+	// Create Spaces here
+	space1 = new Space();
+	space2 = new Space();
+	space3 = new Space();
+	
+	// Create parameters here
+	String noMatches = "Card";
+	String param1 = "";
+	String param2 = "";
+	
+	// Test empty list of Spaces (this shouldn't happen in reality)
+	@Test
+	private void testEmptyTitleSearch() {
+		List<Space> emptyTitleList = new ArrayList<Space>();
+		assertEquals(emptyTitleList, SpaceServiceImpl.titleSearch(noMatches, emptyTitleList));
+	}
+	
+	// Test empty string as parameter expect full list returned
+	@Test
+	private void testEmptyParamTitleSearch() {
+		List<Space> titleList = new ArrayList<Space>();
+		titleList.add(space1);
+		assertEquals(titleList, SpaceServiceImpl.titleSearch("", titleList));
+	}
+	
+	@Test
+	private void testOneTitleSearch() {
+		List<Space> titleList = new ArrayList<Space>();
+		titleList.add(space1);
+		titleList.add(space2);
+		titleList.add(space3);
+		List<Space> resultList = new ArrayList<Space>();
+		resultList.add(space1);
+		assertEquals(resultList, SpaceServiceImpl.titleSearch(param1, titleList));
+	}
+	
+	@Test
+	private void testTwoTitleSearch() {
+		List<Space> titleList = new ArrayList<Space>();
+		titleList.add(space1);
+		titleList.add(space2);
+		titleList.add(space3);
+		List<Space> resultList = new ArrayList<Space>();
+		resultList.add(space1);
+		resultList.add(space2);
+		assertEquals(resultList, SpaceServiceImpl.titleSearch(param2, titleList));
+	}
+	
+	@Test
+	private void testInvalidTitleSearch() {
+		List<Space> titleList = new ArrayList<Space>();
+		titleList.add(space1);
+		List<Space> resultList = new ArrayList<Space>();
+		assertEquals(resultList, SpaceServiceImpl.titleSearch(noMatches, titleList));
+	}
+}
